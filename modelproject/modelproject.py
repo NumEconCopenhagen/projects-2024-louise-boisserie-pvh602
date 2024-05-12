b11e94ce968c586ab19aa31c89e7f681901244be
LouiseBoisserie <144999331+LouiseBoisserie@users.noreply.github.com>
2024-05-12 21:51:56 +0200

    Add files via upload

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,19 +1,15 @@
-from scipy import optimize
 import numpy as np
 
-def solve_backwards(beta,W,T,W_now):
-    # Initialize
-    Vstar = np.nan+np.zeros([W+1,T])
-    Cstar = np.nan + np.zeros([W+1,T])
+def solve_backwards(beta,W,T):
+    # Initializing
+    Vstar = np.nan+np.zeros((W+1,T))
+    Cstar = np.nan + np.zeros((W+1,T))
     Cstar[:,T-1] = np.arange(W+1) 
     Vstar[:,T-1] = np.sqrt(Cstar[:,T-1])
     C_backwards = np.empty(T)
 
-    # Solve
-    # loop over periods
+    # Solving
     for t in range(T-2, -1, -1):  
-        
-        #loop over states
         for w in range(W+1):
             c = np.arange(w+1)
             w_c = w - c
@@ -22,6 +18,8 @@ def solve_backwards(beta,W,T,W_now):
             Vstar[w,t] = np.amax(V_guess)
             Cstar[w,t] = np.argmax(V_guess)
     
+    # Simulating 
+    W_now = W
     for t in range(T):
         W_now = int(W_now)  
         C_backwards[t] = Cstar[W_now,t]  
