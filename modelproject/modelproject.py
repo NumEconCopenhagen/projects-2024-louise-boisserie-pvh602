2c38ae68465463f742918879e73bb741fc512d5a
LouiseBoisserie <144999331+LouiseBoisserie@users.noreply.github.com>
2024-05-12 22:20:31 +0200

    Add files via upload

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -26,3 +26,23 @@ def solve_backwards(beta,W,T):
         W_now = W_now-C_backwards[t] 
     
     return C_backwards
+
+def solve_VFI(W, beta, max_iter=500, tol=1e-3):
+    grid_W = np.arange(W + 1)
+    Cstar = np.zeros(W + 1)
+    V_now = np.zeros(W + 1)
+    it = 0
+    delta = tol + 1
+    
+    while (it <= max_iter and delta > tol):
+        it = it + 1
+        V_next = V_now.copy()
+        for w in range(W + 1):
+            c = np.arange(w + 1)
+            w_next = w - c
+            V_guess = np.sqrt(c) + beta * V_next[w_next]
+            V_now[w] = np.amax(V_guess)
+            Cstar[w] = np.argmax(V_guess)
+        delta = np.amax(np.abs(V_now - V_next))
+    
+    return Cstar
